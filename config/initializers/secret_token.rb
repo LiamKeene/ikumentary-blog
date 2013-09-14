ab7fc0bd999519f0c64549b66249ea73cfcbad32
Liam Keene <liam.keene@gmail.com>
2013-09-14 23:57:51 +1000

    Initial commit of ikumentary blog
     new file:   lots of files!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ikumentary::Application.config.secret_key_base = '0f314135725b6d893eace1c699bdb4ae8b00c5767e9bf1855a089a4e4dcf32ca582b82b53f30ccc2fab446f4e795b0e138552ce6d6ccd6397e4cd6f61ff401b2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ikumentary::Application.config.session_store :cookie_store, key: '_ikumentary_session'
