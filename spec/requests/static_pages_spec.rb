64fc372fabbc31e82834824cf05401664d7c952b
Liam Keene <liam.keene@gmail.com>
2013-09-15 09:35:26 +1000

    Refactors the static page specs
     modified:   spec/requests/static_pages_spec.rb

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,47 +2,27 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
+  subject { page }
+
   describe "Home Page" do
+    before { visit root_path }
     
-    it "should have the content 'Ikumentary'" do
-      visit root_path
-      expect(page).to have_content('Ikumentary')
-    end
-
-    it "should have the base title" do
-      visit root_path
-      expect(page).to have_title('Ikumentary')
-    end
-
-    it "should not have a custom page title" do
-      visit root_path
-      expect(page).not_to have_title('Home |')
-    end
+    it { should have_content('Ikumentary') }
+    it { should have_title('Ikumentary') }
+    it { should_not have_title('Home |') }
   end
 
   describe "About Page" do
-
-    it "should have the content 'About'" do
-      visit about_path
-      expect(page).to have_content('About')
-    end
-  
-    it "should have the title 'About'" do
-      visit about_path
-      expect(page).to have_title('About | Ikumentary')
-    end
+    before { visit about_path }
+    
+    it { should have_content('About') }
+    it { should have_title('About | Ikumentary') }
   end
 
   describe "Contact Page" do
-
-    it "should have the content 'Contact'" do
-      visit contact_path
-      expect(page).to have_content('Contact')
-    end
-  
-    it "should have the title 'Contact'" do
-      visit contact_path
-      expect(page).to have_title('Contact | Ikumentary')
-    end
+    before { visit contact_path }
+    
+    it { should have_content('Contact') }
+    it { should have_title('Contact | Ikumentary') }
   end
 end
