6649c6d2318ca3755823acf109e62c13915fc096
Liam Keene <liam.keene@gmail.com>
2013-09-15 09:20:25 +1000

    Changes specs for the home page view
     modified:   spec/requests/static_pages_spec.rb - Home page doesn't have a custom title
     modified:   app/views/static_pages/home.html.erb - pass new test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,9 +9,14 @@ describe "StaticPages" do
       expect(page).to have_content('Ikumentary')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      expect(page).to have_title('Home | Ikumentary')
+      expect(page).to have_title('Ikumentary')
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('Home |')
     end
   end
 
