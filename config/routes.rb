0dc73e365e162a92a718252c9aac664caadabd78
Liam Keene <liam.keene@gmail.com>
2013-09-15 09:23:44 +1000

    Updates RSpec tests with named routes
     modified:   config/routes.rb
     modified:   spec/requests/static_pages_spec.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Ikumentary::Application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/about'
-  get 'static_pages/contact'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'static_pages#home'
+
+  match '/about',     to: 'static_pages#about',     via: 'get'
+  match '/contact',   to: 'static_pages#contact',   via: 'get'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,17 +5,17 @@ describe "StaticPages" do
   describe "Home Page" do
     
     it "should have the content 'Ikumentary'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_content('Ikumentary')
     end
 
     it "should have the base title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_title('Ikumentary')
     end
 
     it "should not have a custom page title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).not_to have_title('Home |')
     end
   end
@@ -23,12 +23,12 @@ describe "StaticPages" do
   describe "About Page" do
 
     it "should have the content 'About'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content('About')
     end
   
     it "should have the title 'About'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title('About | Ikumentary')
     end
   end
@@ -36,12 +36,12 @@ describe "StaticPages" do
   describe "Contact Page" do
 
     it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_content('Contact')
     end
   
     it "should have the title 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_title('Contact | Ikumentary')
     end
   end
