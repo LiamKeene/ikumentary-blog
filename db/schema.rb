d68000dbbb71d659159c2a4fe0dbffb32d8cd830
Liam Keene <liam.keene@gmail.com>
2013-09-17 00:08:59 +1000

    Creates an index on the User email and login, which are unique
     new file:   db/migrate/20130916135106_add_index_to_users_email_login.rb - migration to add index
     modified:   db/schema.rb - resulting schema
    
     modified:   app/models/user.rb - enforce consistent case in emails before saving

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130916111143) do
+ActiveRecord::Schema.define(version: 20130916135106) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -24,5 +24,8 @@ ActiveRecord::Schema.define(version: 20130916111143) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["login"], name: "index_users_on_login", unique: true
 
 end
