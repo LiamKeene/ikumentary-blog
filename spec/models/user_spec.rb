eb90bd46160420f37bb1448d9234c4f8f520db97
Liam Keene <liam.keene@gmail.com>
2013-09-16 21:30:56 +1000

    Simple TDD spec for the User model
     modified:   spec/models/user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before { @user = User.new(name: 'Example User', display_name: 'Example', 
+    email: 'user@example.com', url: 'example.com', login: 'example', 
+    password: 'password', user_type: 'administrator') }
+
+  subject { @user }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:display_name) }
+  it { should respond_to(:email) }
+  it { should respond_to(:url) }
+  it { should respond_to(:login) }
+  it { should respond_to(:password) }
+  it { should respond_to(:user_type) }
+
 end
