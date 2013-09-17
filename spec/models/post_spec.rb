ad69b91bff14a0ef79831b5b1f228df6940ba076
Liam Keene <liam.keene@gmail.com>
2013-09-17 14:01:16 +1000

    Adds specs for the Post model
     modified:   spec/models/post_spec.rb

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe Post do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before { @post = Post.new(title: 'First Post', slug: 'first-post', 
+    content: 'This is the content of the first post', 
+    author_id: 1, status: 'Published') }
+
+  subject { @post }
+
+  it { should respond_to(:title) }
+  it { should respond_to(:slug) }
+  it { should respond_to(:content) }
+  it { should respond_to(:author_id) }
+  it { should respond_to(:status) }
 end
