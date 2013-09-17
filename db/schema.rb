3c2ba420840eeebc3f52097c0346c4cdcfc07317
Liam Keene <liam.keene@gmail.com>
2013-09-17 13:57:09 +1000

    Adds Post model, migration and DB schema
     new file:   app/models/post.rb
     new file:   db/migrate/20130917035611_create_posts.rb
     modified:   db/schema.rb
     new file:   spec/models/post_spec.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130917022709) do
+ActiveRecord::Schema.define(version: 20130917035611) do
+
+  create_table "posts", force: true do |t|
+    t.string   "title"
+    t.string   "slug"
+    t.text     "content"
+    t.integer  "author_id"
+    t.string   "status"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
