8181fb2505530e7c7a3b7a9bccf50b2f494d1e6b
Liam Keene <liam.keene@gmail.com>
2013-09-17 13:06:30 +1000

    Shortens email downcasing in User model
     modified:   app/models/user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  before_save { self.email = email.downcase }
+  before_save { email.downcase! }
   validates :name,      presence: true, length: { maximum: 40 }
   validates :email,     presence: true, uniqueness: { case_sensitive: false }
   validates :login,     length: { minimum: 4, maximum: 40 }, uniqueness: true
