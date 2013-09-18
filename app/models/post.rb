231776edbf571726f47195da961c5038c8ac9685
Liam Keene <liam.keene@gmail.com>
2013-09-19 08:59:54 +1000

    Implements the has_many/belongs_to association between Users and Posts
     modified:   app/models/post.rb
     modified:   app/models/user.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,6 @@ class Post < ActiveRecord::Base
   validates :slug,        presence: true, length: { maximum: 100 }, uniqueness: { case_sensitive: false }
   validates :content,     presence: true
   validates :author_id,   presence: true
+  
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ActiveRecord::Base
   before_save { email.downcase! }
-  
+
   validates :name,      presence: true, length: { maximum: 40 }
   validates :email,     presence: true, uniqueness: { case_sensitive: false }
   validates :login,     length: { minimum: 4, maximum: 40 }, uniqueness: true
   validates :password,  length: { minimum: 6 }
-  
+
+  has_many :posts, foreign_key: 'author_id'
+    
   has_secure_password
 end
