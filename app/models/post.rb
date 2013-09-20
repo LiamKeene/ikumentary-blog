2180a3b16afe555ff76dee6b2a4d6294e11452c3
Liam Keene <liam.keene@gmail.com>
2013-09-20 17:32:14 +1000

    Adds scope to return latest posts and dependency to destrot Posts to User model
     modified:   app/models/post.rb
     modified:   app/models/user.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,10 @@
 class Post < ActiveRecord::Base
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+
+  scope :latest, -> { order('created_at DESC') }
+  
   validates :title,       presence: true, length: { maximum: 255 }
   validates :slug,        presence: true, length: { maximum: 100 }, uniqueness: { case_sensitive: false }
   validates :content,     presence: true
   validates :author_id,   presence: true
-  
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 end
