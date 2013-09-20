978e3b96e23ccf5fbead7d53c81f6e182e82805d
Liam Keene <liam.keene@gmail.com>
2013-09-20 20:57:34 +1000

    Passes the new Comment specs and Post/Comment assocation specs
     modified:   app/models/comment.rb
     modified:   app/models/post.rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,8 @@
 class Comment < ActiveRecord::Base
+  belongs_to :post
+
+  validates :post_id, presence: true
+  validates :author,  presence: true, length: { maximum: 100 }
+  validates :email,   presence: true
+  validates :content, presence: true
 end
