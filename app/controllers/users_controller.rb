c2de3d8145cd8766b8c513e0795c93f62ac4f872
Liam Keene <liam.keene@gmail.com>
2013-09-21 17:57:47 +1000

    Adds a create action and defines parameters that can be POST'ed
     modified:   app/controllers/users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,17 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def create
+    @user = User.new(user_params)
+
+    @user.save
+    redirect_to @user
+  end
+
+  private
+    # Never trust params from the interwebs!  Only allow white-listed params
+    def user_params
+      params.require(:user).permit(:name, :display_name, :email, :url, :login, :password, :password_confirmation, :user_type)
+    end
 end
