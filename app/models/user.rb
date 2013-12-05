ed6bf121915227299471aeb0b4442c861d84c375
Liam Keene <liam.keene@gmail.com>
2013-12-05 23:13:57 +1100

    Adds remember_token field to users table, and updates specs
     new file:   db/migrate/20131205121032_add_remember_token_to_users.rb
     modified:   db/schema.rb
    
     modified:   app/models/user.rb - defines a method to create the token
     modified:   spec/models/user_spec.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  before_create :create_remember_token
   before_save { email.downcase! }
 
   has_many :posts, foreign_key: 'author_id', dependent: :destroy
@@ -9,4 +10,17 @@ class User < ActiveRecord::Base
   validates :email,     presence: true, uniqueness: { case_sensitive: false }
   validates :login,     length: { minimum: 4, maximum: 40 }, uniqueness: true
   validates :password,  length: { minimum: 6 }
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+    def create_remember_token
+      self.remember_token = User.encrypt(User.new_remember_token)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130920075248) do
+ActiveRecord::Schema.define(version: 20131205121032) do
 
   create_table "comments", force: true do |t|
     t.integer  "post_id"
@@ -44,9 +44,11 @@ ActiveRecord::Schema.define(version: 20130920075248) do
     t.string   "user_type"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "remember_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["login"], name: "index_users_on_login", unique: true
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
