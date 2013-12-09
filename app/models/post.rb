f9ee4bf59a299d749ded3c92b09742d17745abb1
Liam Keene <liam.keene@gmail.com>
2013-12-09 21:07:14 +1100

    Adds Kaminari gor pagination and implements paging of Posts
     modified:   Gemfile
     modified:   Gemfile.lock
     modified:   app/controllers/posts_controller.rb - paginates using page param
     modified:   app/models/post.rb - defines number of posts per page
     modified:   app/views/posts/index.html.erb - adds pager links
     modified:   config/routes.rb - defines a route so that page urls are pretty
                  (posts/page/2 and not posts/?page=2)

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,8 @@ class Post < ActiveRecord::Base
 
   scope :latest, -> { order('created_at DESC') }
 
+  paginates_per 5
+
   validates :title,       presence: true, length: { maximum: 255 }
   validates :slug,        presence: true, length: { maximum: 100 }, uniqueness: { case_sensitive: false }
   validates :content,     presence: true
