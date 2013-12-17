a69679db14b02ddd38b971a1602d0171297d67ad
Liam Keene <liam.keene@gmail.com>
2013-12-17 20:06:52 +1100

    Installs friendly_id gem to create nice URLs for Posts
     modified:   Gemfile
     modified:   Gemfile.lock
    
     new file:   config/initializers/friendly_id.rb
     new file:   db/migrate/20131217002247_create_friendly_id_slugs.rb
     modified:   db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131214212026) do
+ActiveRecord::Schema.define(version: 20131217002247) do
 
   create_table "comments", force: true do |t|
     t.integer  "post_id"
@@ -25,6 +25,19 @@ ActiveRecord::Schema.define(version: 20131214212026) do
     t.string   "agent"
   end
 
+  create_table "friendly_id_slugs", force: true do |t|
+    t.string   "slug",                      null: false
+    t.integer  "sluggable_id",              null: false
+    t.string   "sluggable_type", limit: 50
+    t.string   "scope"
+    t.datetime "created_at"
+  end
+
+  add_index "friendly_id_slugs", ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+  add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+  add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
+  add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
+
   create_table "posts", force: true do |t|
     t.string   "title"
     t.string   "slug"
@@ -36,6 +49,13 @@ ActiveRecord::Schema.define(version: 20131214212026) do
   end
 
   add_index "posts", ["slug"], name: "index_posts_on_slug", unique: true
+
+  create_table "tests", force: true do |t|
+    t.string   "field1"
+    t.string   "field2"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
