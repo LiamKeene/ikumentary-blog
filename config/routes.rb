5f3eda0d8ae27f8dad815df34a4a825bc8fcd076
Liam Keene <liam.keene@gmail.com>
2013-12-17 20:35:45 +1100

    Changes root url to posts#index action
     modified:   config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Ikumentary::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'static_pages#home'
+  root 'posts#index'
 
   match '/about',     to: 'static_pages#about',     via: 'get'
   match '/contact',   to: 'static_pages#contact',   via: 'get'
