7174b8d3f99735076bc897ea349e544b80a55784
Liam Keene <liam.keene@gmail.com>
2014-01-02 16:40:38 +1100

    Migrates the Post.published_at field from string to datetime
     new file:   db/migrate/20140101124026_change_posts_published_at_to_datetime.rb
                  defines an up/down migration to change data from a string to datetime
     modified:   db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140101122053) do
+ActiveRecord::Schema.define(version: 20140101124026) do
 
   create_table "comments", force: true do |t|
     t.integer  "post_id"
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 20140101122053) do
     t.string   "slug"
     t.text     "content"
     t.integer  "author_id"
-    t.string   "published_at"
+    t.datetime "published_at"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
