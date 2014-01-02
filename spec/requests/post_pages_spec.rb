074f2e72ffef0096ed4b048237455b1c4d72e7b3
Liam Keene <liam.keene@gmail.com>
2014-01-02 20:43:02 +1100

    Increases requests specs for the public Post views to handle draft/published Posts
     modified:   spec/requests/post_pages_spec.rb

--- spec/requests/post_pages_spec.rb
+++ spec/requests/post_pages_spec.rb
@@ -1,16 +1,28 @@
 require 'spec_helper'
 
-describe "Post Pages" do
+describe 'Post Pages' do
 
   subject { page }
 
-  describe "index" do
-    let(:posts) { FactoryGirl.create_list(:post, 5, :published) }
-    before { visit posts_path }
+  describe 'index' do
 
-    it "should list each post" do
-      Post.all.each do |post|
-        expect(page).to have_selector('div', text: post.title)
+    context 'published posts' do
+      let!(:posts) { FactoryGirl.create_list(:post, 5, :published) }
+      before { visit posts_path }
+
+      it 'lists each post' do
+        Post.all.each do |post|
+          expect(page).to have_selector('div', text: post.title)
+        end
+      end
+    end
+
+    context 'draft posts' do
+      let!(:draft) { FactoryGirl.create(:post, :draft) }
+      before { visit posts_path }
+
+      it 'does not list drafts' do
+        expect(page).to_not have_selector('div', text: draft.title)
       end
     end
   end
