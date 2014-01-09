6ebae9dddafd29208c301e574b45a201d63323c3
Liam Keene <liam.keene@gmail.com>
2014-01-09 12:24:18 +1100

    Creates superclass model Article, which Post inherits - model specs now pass again
     renamed:    app/models/post.rb -> app/models/article.rb
     modified:   app/models/comment.rb
     modified:   app/models/post.rb

---
+++ app/models/article.rb
@@ -0,0 +1,20 @@
+class Article < ActiveRecord::Base
+
+  extend FriendlyId
+
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+
+  friendly_id :slug, use: :slugged
+
+  has_many :comments, dependent: :destroy
+
+  scope :latest, -> { order('created_at DESC') }
+  scope :published, -> { where('published_at < ?', Time.now).order('published_at DESC') }
+
+  paginates_per 5
+
+  validates :title,       presence: true, length: { maximum: 255 }
+  validates :slug,        presence: true, length: { maximum: 100 }, uniqueness: { case_sensitive: false }
+  validates :content,     presence: true
+  validates :author_id,   presence: true
+end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ActiveRecord::Base
-  belongs_to :post
+  belongs_to :article
 
-  validates :post_id, presence: true
+  validates :article_id, presence: true
   validates :author,  presence: true, length: { maximum: 100 }
   validates :email,   presence: true
   validates :content, presence: true
