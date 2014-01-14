a8f335814230511745823a8b8dac626d57bb2f84
Liam Keene <liam.keene@gmail.com>
2014-01-15 09:21:49 +1100

    Creates many-to-many association table for Articles and Tags
     new file:   db/migrate/20140114210624_create_articles_tags.rb
     modified:   db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140114055744) do
+ActiveRecord::Schema.define(version: 20140114210624) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -26,6 +26,11 @@ ActiveRecord::Schema.define(version: 20140114055744) do
 
   add_index "articles", ["published_at"], name: "index_articles_on_published_at"
   add_index "articles", ["slug"], name: "index_articles_on_slug", unique: true
+
+  create_table "articles_tags", force: true do |t|
+    t.integer "article_id"
+    t.integer "tag_id"
+  end
 
   create_table "comments", force: true do |t|
     t.integer  "article_id"
