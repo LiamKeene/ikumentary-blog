0fe15a3474364d3a54af77a49283d9643386c9a5
Liam Keene <liam.keene@gmail.com>
2014-01-15 09:22:44 +1100

    Adds relations to Article/Tag models to pass specs
     modified:   app/models/article.rb
     modified:   app/models/tag.rb

--- app/models/article.rb
+++ app/models/article.rb
@@ -8,6 +8,8 @@ class Article < ActiveRecord::Base
 
   has_many :comments, dependent: :destroy
 
+  has_and_belongs_to_many :tags
+
   scope :latest, -> { order('created_at DESC') }
   scope :published, -> { where('published_at < ?', Time.now).order('published_at DESC') }
 
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,7 @@
 class Tag < ActiveRecord::Base
 
+  has_and_belongs_to_many :articles, -> { order('created_at DESC') }
+
   validates :display_name,  presence: true, uniqueness: true
 
 end
