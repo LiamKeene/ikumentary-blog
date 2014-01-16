83c306a54f0a8b830087919a92055fa3f3730bda
Liam Keene <liam.keene@gmail.com>
2014-01-16 17:53:58 +1100

    Tag specs now include testing that :name is URL friendly and unique
     modified:   app/models/tag.rb
     modified:   spec/models/tag_spec.rb

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,7 +1,16 @@
 class Tag < ActiveRecord::Base
 
+  before_save :create_tag_name
+
   has_and_belongs_to_many :articles, -> { order('created_at DESC') }
 
-  validates :display_name,  presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true
+
+  def create_tag_name
+    if self.display_name.blank?
+      self.display_name = self.name
+    end
+    self.name = self.display_name.to_url
+  end
 
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Tag do
-  
+
   before { @tag = FactoryGirl.create(:tag) }
 
   subject { @tag }
@@ -10,12 +10,45 @@ describe Tag do
   it { should respond_to(:display_name) }
 
   it { should respond_to(:articles) }
-
+  
   it { should be_valid }
 
-  describe 'when display_name is not present' do
-    before { @tag.display_name = '' }
+  describe 'when name is not present' do
+    before { @tag.name = '' }
     it { should_not be_valid }
   end
 
+  context 'tag creation' do
+
+    describe 'when display name is not specified' do
+      let(:test_name) { 'Cool Stuff' }
+      before do
+        @tag.name = test_name
+        @tag.display_name = ''
+        @tag.save
+      end
+
+      it 'copies the name to the display name' do
+        expect(@tag.display_name).to eq(test_name)
+      end
+
+      it 'then uses the display name to create a URL friendly name' do
+        expect(@tag.name).to eq(@tag.display_name.to_url)
+      end
+    end
+
+    describe 'capital letters and spaces are removed' do
+      let(:simple_tag) { FactoryGirl.create(:tag, display_name: 'SimpleTag') }
+      let(:tag_with_spaces) { FactoryGirl.create(:tag, display_name: 'tag with spaces') }
+
+      it { expect(simple_tag.name).to eq('simpletag') }
+      it { expect(tag_with_spaces.name).to eq('tag-with-spaces') }
+    end
+
+    describe 'tags are unique' do
+      let(:tag_with_same_name) { @tag.dup }
+
+      it { expect(tag_with_same_name).to_not be_valid }
+    end
+  end
 end
