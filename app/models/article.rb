a1c838a33f5cb68efbd0b105a5a709e7a126ef08
Liam Keene <liam.keene@gmail.com>
2014-01-18 09:35:06 +1100

    Updates Article and Category models to pass m2m and extra method specs
     modified:   app/models/article.rb
     modified:   app/models/category.rb

--- app/models/article.rb
+++ app/models/article.rb
@@ -8,6 +8,7 @@ class Article < ActiveRecord::Base
 
   has_many :comments, dependent: :destroy
 
+  has_and_belongs_to_many :categories
   has_and_belongs_to_many :tags
 
   scope :latest, -> { order('created_at DESC') }
