167ffbce05dbc61787753feda8b0151df2db29ac
Liam Keene <liam.keene@gmail.com>
2014-01-18 10:26:58 +1100

    Creates admin CategoriesController and admin Category views
     new file:   app/controllers/admin/categories_controller.rb
     new file:   app/views/admin/categories/_form.html.erb
     new file:   app/views/admin/categories/edit.html.erb
     new file:   app/views/admin/categories/index.html.erb
     new file:   app/views/admin/categories/new.html.erb
     new file:   app/views/admin/categories/show.html.erb

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,52 @@
+class Admin::CategoriesController < ApplicationController
+
+  def index
+    @categories = Category.all
+  end
+  
+  def show
+    @category = Category.find(params[:id])
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, @category]
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    
+    if @category.update_attributes(category_params)
+      redirect_to [:admin, @category]
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+
+    redirect_to [:admin, :categories]
+  end
+
+  private
+    # Never trust params from the interwebs!  Only allow white-listed params
+    def category_params
+      params.require(:category).permit(:name, :display_name)
+    end
+
+end
