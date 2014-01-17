b038245585436400b77ed3eafacf8dae219253e1
Liam Keene <liam.keene@gmail.com>
2014-01-18 09:13:26 +1100

    Migration to create categories table
     new file:   db/migrate/20140117215517_create_categories.rb
     modified:   db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140114210624) do
+ActiveRecord::Schema.define(version: 20140117215517) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
     t.string   "slug"
     t.text     "content"
     t.integer  "author_id"
-    t.string   "type"
     t.datetime "published_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "type"
   end
 
   add_index "articles", ["published_at"], name: "index_articles_on_published_at"
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(version: 20140114210624) do
   create_table "articles_tags", force: true do |t|
     t.integer "article_id"
     t.integer "tag_id"
+  end
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.string   "display_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "comments", force: true do |t|
