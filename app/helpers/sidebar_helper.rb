2848a8e214670d13ed490a286e5732ff11a5d2e6
Liam Keene <liam.keene@gmail.com>
2014-01-23 22:03:09 +1100

    Creates helper to create array of Tag display name and link hashes
     new file:   app/helpers/sidebar_helper.rb - same method as category_navigation_links
     new file:   spec/helpers/sidebar_helper_spec.rb

--- app/helpers/sidebar_helper.rb
+++ app/helpers/sidebar_helper.rb
@@ -5,4 +5,11 @@ module SidebarHelper
       [category.display_name, category_path(category.name)]
     end
   end
+
+  def tag_navigation_links
+    # Tags with published articles
+    links_array = Tag.with_articles.map do |tag|
+      [tag.display_name, tag_path(tag.name)]
+    end
+  end
 end
--- spec/helpers/sidebar_helper_spec.rb
+++ spec/helpers/sidebar_helper_spec.rb
@@ -33,4 +33,36 @@ describe SidebarHelper do
       expect(actual_links).to eq(expected_links)
     end
   end
+
+  describe 'tag_navigation_links' do
+    let(:ham) { create(:tag, name: 'ham', display_name: 'Ham') }
+    let(:spam) { create(:tag, name: 'spam', display_name: 'Spam') }
+    let(:eggs) { create(:tag, name: 'eggs', display_name: 'Eggs') }
+
+    let(:pub_art1) { create(:article, :published) }
+    let(:pub_art2) { create(:article, :published) }
+    let(:pub_art3) { create(:article, :published) }
+    
+    let(:expected_links) { [] }
+    let(:actual_links) { tag_navigation_links }
+
+    before do
+      # Assign articles
+      ham.articles << pub_art1
+      spam.articles << pub_art2
+      eggs.articles << pub_art3
+
+      expected_links.append(
+        [ham.display_name, tag_path(ham.name)]
+      ).append(
+        [spam.display_name, tag_path(spam.name)]
+      ).append(
+        [eggs.display_name, tag_path(eggs.name)]
+      )
+    end
+
+    it 'returns an array of URL name and path hashes' do
+      expect(actual_links).to eq(expected_links)
+    end
+  end
 end
