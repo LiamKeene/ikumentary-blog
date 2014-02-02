3b13c34b0d53991c80ed778ab9502ee55b9a685f
Liam Keene <liam.keene@gmail.com>
2014-02-02 22:16:28 +1100

    Helper to format a published article's date
     new file:   app/helpers/article_helper.rb
     new file:   spec/helpers/article_helper_spec.rb

---
+++ spec/helpers/article_helper_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe ArticleHelper do
+  
+  describe 'format_date' do
+
+    let(:pub_article) do
+      create(:article, published_at: 'Sat, 25 Jan 2014 11:02:40 UTC +00:00')
+    end
+    let(:pub_date) { format_published_date(pub_article.published_at) }
+      
+    let(:draft_article) { create(:article, :draft) }
+    let(:draft_date) { format_published_date(draft_article.published_at) }
+    
+    it 'formats the published_at date of an article' do
+      expect(pub_date).to eq('25/01/2014')
+    end
+
+    it 'returns "Unpublished" if the article is a draft' do
+      expect(draft_date).to eq('Unpublished')
+    end
+  end
+end
