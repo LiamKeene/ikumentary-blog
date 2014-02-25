763aa76c9d73a51bed684f4b98cca741fccd1122
Liam Keene <liam.keene@gmail.com>
2014-02-25 16:57:47 +1100

    Adds a new ArticleHelper to format the `datetime` attribute of HTML5 time elements
     modified:   app/helpers/article_helper.rb - formats date with a suitable format, or
                    an empty string for drafts
     modified:   spec/helpers/article_helper_spec.rb

--- app/helpers/article_helper.rb
+++ app/helpers/article_helper.rb
@@ -4,4 +4,10 @@ module ArticleHelper
   def format_published_date(date)
     !date.nil? ? date.strftime('%d/%m/%Y') : 'Unpublished'
   end
+  # Formats the published date of an article according to the specs laid out
+  # for the `datetime` attribute of the HTML5 time element.  If the article is
+  # a draft and empty string is returned
+  def format_html5_time(date)
+    !date.nil? ? date.strftime('%Y-%m-%d %H:%M:%S%z') : ''
+  end
 end
--- spec/helpers/article_helper_spec.rb
+++ spec/helpers/article_helper_spec.rb
@@ -1,15 +1,12 @@
 require 'spec_helper'
 
 describe ArticleHelper do
+
+  let(:pub_article) { create(:article, published_at: 'Sat, 25 Jan 2014 11:02:40 UTC +00:00') }
+  let(:draft_article) { create(:article, :draft) }
   
   describe 'format_published_date' do
-
-    let(:pub_article) do
-      create(:article, published_at: 'Sat, 25 Jan 2014 11:02:40 UTC +00:00')
-    end
     let(:pub_date) { format_published_date(pub_article.published_at) }
-      
-    let(:draft_article) { create(:article, :draft) }
     let(:draft_date) { format_published_date(draft_article.published_at) }
     
     it 'formats the published_at date of an article' do
@@ -18,6 +15,20 @@ describe ArticleHelper do
 
     it 'returns "Unpublished" if the article is a draft' do
       expect(draft_date).to eq('Unpublished')
+    end
+  end
+
+  describe 'format_html5_time' do
+    let(:pub_date) { format_html5_time(pub_article.published_at) }
+    let(:draft_date) { format_html5_time(draft_article.published_at) }
+
+    it 'formats the published_at date to HTML5 time element spec' do
+      expect(pub_date).to eq('2014-01-25 11:02:40+0000')
+    end
+
+    # HTML5 spec has a very strict subset of allowed times
+    it 'returns an empty string if the article us a draft' do
+      expect(draft_date).to eq('')
     end
   end
 end
