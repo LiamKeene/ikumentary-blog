8e3295197b38e692c50ffe9c9b0df155e7608aa2
Liam Keene <liam.keene@gmail.com>
2014-03-05 20:22:35 +1100

    Migration to create settings table
     new file:   app/models/settings.rb
     new file:   db/migrate/20140305085836_create_settings.rb
     modified:   db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140219102027) do
+ActiveRecord::Schema.define(version: 20140305085836) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -70,6 +70,17 @@ ActiveRecord::Schema.define(version: 20140219102027) do
   add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
   add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
   add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
+
+  create_table "settings", force: true do |t|
+    t.string   "var",                   null: false
+    t.text     "value"
+    t.integer  "thing_id"
+    t.string   "thing_type", limit: 30
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "settings", ["thing_type", "thing_id", "var"], name: "index_settings_on_thing_type_and_thing_id_and_var", unique: true
 
   create_table "tags", force: true do |t|
     t.string   "name"
