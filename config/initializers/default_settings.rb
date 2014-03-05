133008c6fb36d57ffeb8e4f4604e425f7be19501
Liam Keene <liam.keene@gmail.com>
2014-03-05 20:49:28 +1100

    Creates some default settings for the blog
     new file:   config/initializers/default_settings.rb

---
+++ config/initializers/default_settings.rb
@@ -0,0 +1,6 @@
+Settings.defaults['blog.title'] = 'ikumentary'
+Settings.defaults['blog.subtitle'] = 'parenting in japanese in australia'
+Settings.defaults['blog.about'] = 'Ikumentary is a blog that documents a new 
+father\'s attempt to raise his children using the Japanese language, while 
+living in Australia.<br />
+Oh, did I mention he is not Japanese?'
